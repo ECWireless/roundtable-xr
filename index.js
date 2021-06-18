08945a137c2e4dffa216c32845867e9cd1f40453
ECWireless <Elliott@valtechcreative.com>
2021-06-18 13:06:39 -0600

    Add test args

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const dotenv = require('dotenv');
 const Discord = require('discord.js');
 
+const PREFIX = '!';
+
 dotenv.config();
 const client = new Discord.Client();
 
@@ -9,9 +11,26 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (message.content === '!ping') {
-		// send back "Pong." to the channel the message was sent in
+	if (!message.content.startsWith(PREFIX) || message.author.bot) return;
+
+	const args = message.content.slice(PREFIX.length).trim().split(/ +/);
+	const command = args.shift().toLowerCase();
+
+	if (command === 'ping') {
 		message.channel.send('Pong.');
+	}
+	else if (command === 'avatar') {
+		if (!message.mentions.users.size) {
+			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL({ format: 'png', dynamic: true })}>`);
+		}
+
+		const avatarList = message.mentions.users.map(user => {
+			return `${user.username}'s avatar: <${user.displayAvatarURL({ format: 'png', dynamic: true })}>`;
+		});
+
+		// Send the entire array of strings as a message
+		// By default, discord.js will `.join()` the array with `\n`
+		message.channel.send(avatarList);
 	}
 });
 
