1e11a5732ab7d570a54eca48377a1130c0ba54da
ECWireless <Elliott@valtechcreative.com>
2021-06-18 12:10:53 -0600

    Add test ping pong message

--- index.js
+++ index.js
@@ -1,4 +1,18 @@
 const dotenv = require('dotenv');
-dotenv.config();
+const Discord = require('discord.js');
 
-console.log(process.env.TOKEN);
+dotenv.config();
+const client = new Discord.Client();
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('message', message => {
+	if (message.content === '!ping') {
+		// send back "Pong." to the channel the message was sent in
+		message.channel.send('Pong.');
+	}
+});
+
+client.login(process.env.TOKEN);
