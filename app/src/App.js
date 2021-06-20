be8ae7bc66f79c8da1eaf20a0f70f542787a72ac
ECWireless <Elliott@valtechcreative.com>
2021-06-20 12:48:07 -0600

    Create template react app

---
+++ app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Congrats! You have created an XR room version of #$channel
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
