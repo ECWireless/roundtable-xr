acca948422b1f2704aa37f2a24d9017710f2bbec
ECWireless <Elliott@valtechcreative.com>
2021-06-22 12:16:50 -0600

    Add tingle

--- commands/create.js
+++ commands/create.js
@@ -12,6 +12,7 @@ module.exports = {
 	description: 'Allows you to create a WebXR channel.',
 	execute(message, args) {
 		console.log(args);
+		message.channel.send('Creating...');
 		createRoom(message.guild.name, message.channel.name, message.guild.memberCount).then(url => {
 			message.channel.send(`Room created at https://arweave.net/${url}`);
 			message.channel.send(`You created an XR version of the channel "#${message.channel.name}".`);
